af6a2d9f301806996de1fb097bc9e7b22bcf07af
Kapelek <kacperkedzior@interia.eu>
2026-06-21 11:51:07 +0200

    params and search params / use utility to use them from client

--- routing-demo/app/page.tsx
+++ routing-demo/app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
     <>
       <h1>Welcome home!</h1>
       <Link href="/blog">Go To Blog</Link>
+      <Link href="/products">Go To Products</Link>
+      <Link href="articles/breaking-news-123?lang=en">Read In English</Link>
+      <Link href="articles/breaking-news-123?lang=fr">Read In French</Link>
     </>
   );
 }
